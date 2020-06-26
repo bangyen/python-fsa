dbb90bac2f16019e48aee2b3f3d483636a320b0c
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-06-25 22:52:26 -0400

    Update automaton.py

--- src/automaton.py
+++ src/automaton.py
@@ -6,6 +6,7 @@ class StateMach:
         """
         Initializes the StateMach object with its FSA, sets the initial state,
         sets the initial acceptance value, then normalizes it.
+        
         :param fsa: The dictionary representation of the FSA.
         """
         self.fsa = fsa
@@ -26,6 +27,7 @@ class StateMach:
         """
         Accepts an input which is passed through the FSA,
         changing the current state and whether the word is accepted.
+        
         :param arg: Either an int, multiple ints, or a list of ints.
         :return: Returns self so as to allow multiple calls.
         """
@@ -40,6 +42,7 @@ class StateMach:
         """
         Creates a table, where the left-hand side is the current state
         and the right hand side is the associated information.
+        
         :return: Returns a more readable version of the FSA.
         """
         dct = "\n".join(f"{key}: {val}" for key, val in self.fsa.items())
